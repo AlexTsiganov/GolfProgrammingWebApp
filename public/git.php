8d41870664524e5d066524ee0b3271552c9a39b0
Alex Tsiganov <alexandr.tsiganov@gmail.com>
2015-11-18 18:45:34 +0300

    test webhooks

--- public/git.php
+++ public/git.php
@@ -2,6 +2,7 @@
 
 $DESIRED_BRANCH     = "deploy";
 
+echo shell_exec("echo hello alex");
 echo shell_exec("forever stopall");
 echo shell_exec("git pull && git checkout {$BRANCH} && forever start bin/www");
 
