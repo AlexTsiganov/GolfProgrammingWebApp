a4c88bb9784d80f976e5b6f082fe26ca1b81400e
Artem Pereshein <pereshein.artem@gmail.com>
2015-11-22 00:15:18 +0300

    added error handling and example of using test system

--- testing_system/testing.js
+++ testing_system/testing.js
@@ -1,48 +1,44 @@
-// connect to "virtual" database
-var db = require('./database');
+// required modules
+var db = require('./database');             // connect to "virtual" database
+var events = require('events');             // for events handling
+var exec = require('child_process').exec;   // for execute external code
+var fs = require('fs');                     // for work with files
 
-// for events handling
-var events = require('events');
-var eventEmitter = new events.EventEmitter();
+// constructor of the class TestSystem
+var TestSystem = function TestSystem(){
+    events.EventEmitter.call(this);
+	this.n_tests = 3;
+};
+require('util').inherits(TestSystem, events.EventEmitter);
 
-// for execute external code
-var exec = require('child_process').exec;
+// fields of the class
+TestSystem.code;        // executable file
+TestSystem.n_tests;     // number of test cases
+TestSystem.tests;       // test cases
+TestSystem.exec_command;// command to execute client's code
+TestSystem.timelimit;   // timelimit for current task
+TestSystem.solution;    // solution ID from database
 
-// for work with files
-var fs = require('fs');
-var workpath = '/home/adminuser/golf_app/'; // !!!!!!
-var input = workpath + 'input.txt';
-var output = workpath + 'output.txt';
-var etalon = workpath + 'etalon.txt';
-var code = workpath + 'main';  // executable file
-
-// status of solution (testing, runtime error, exceeding time limit, OK)
-var status;
-
-// number of test cases
-var n_tests = 3;
-
-// information from database
-var tests;
-var exec_command;
-var timelimit;
+// object of class for events handling
+var test_system = new TestSystem();
 
 /**
  * Main function for testing client's code.
- * @param solution - solution ID from database.
- * @param filename - full name (with path) of the file with code.
+ * This function starts the test - calls the function writeInput().
+ * @param {type} solution - solution ID from database.
+ * @param {type} filename - full name (with path) of the file with code.
  * @returns {undefined}
  */
-function testing(solution, filename) {
-
-    // status update
-    status = 'testing';
+TestSystem.prototype.testing = function testing(solution, filename) {
+    
+    // save solution ID
+    this.solution = solution;
     
     // save filename
-    code = filename;
+    this.code = filename;
     
     // select from DB
-    tests = [
+    this.tests = [
         {
             input: db.tests[0].input_data,
             output: db.tests[0].output_data
@@ -56,99 +52,112 @@ function testing(solution, filename) {
             output: db.tests[2].output_data
         }
     ];
-    exec_command = db.langs[0].exec_command;
-    timelimit = db.tasks[0].timelimit;
+    this.exec_command = db.langs[0].exec_command;
+    this.timelimit = db.tasks[0].timelimit;
     
     // start test with 1st test case
-    writeInput(1);
+    this.writeInput(1);
 
-}
+};
 
 /**
  * Function for write input test in file. Completion of the function generates 
- * the event. This event triggers the function writeEtalon().
- * @param run - number of current test case (1, 2, 3, ...).
+ * the event 'inputIsWrited'. This event calls the function writeEtalon().
+ * @param {type} run - number of current test case (1, 2, 3, ...).
  * @returns {undefined}
  */
-var writeInput = function(run) {
-    fs.writeFile(input, tests[run-1].input, function(){
-	eventEmitter.emit('inputIsWrited', run);
+TestSystem.prototype.writeInput = function writeInput(run) {
+    fs.writeFile('input.txt', this.tests[run-1].input, function(){
+	test_system.emit('inputIsWrited', run);
     });
 };
-eventEmitter.on('inputIsWrited', function(run){
-    console.log('input.txt is writed');
-    writeEtalon(run);
+test_system.on('inputIsWrited', function(run){
+    //console.log('input.txt is writed');
+    this.writeEtalon(run);
 });
 
 /**
  * Function for write output test in file. Completion of the function generates 
- * the event. This event triggers the function execCode().
- * @param run - number of current test case (1, 2, 3, ...).
+ * the event 'etalonIsWrited'. This event calls the function execCode().
+ * @param {type} run - number of current test case (1, 2, 3, ...).
  * @returns {undefined}
  */
-var writeEtalon = function(run) {
-    fs.writeFile(etalon, tests[run-1].output, function(){
-	eventEmitter.emit('etalonIsWrited', run);
+TestSystem.prototype.writeEtalon = function(run) {
+    fs.writeFile('etalon.txt', this.tests[run-1].output, function(){
+	test_system.emit('etalonIsWrited', run);
     });
 };
-eventEmitter.on('etalonIsWrited', function(run){
-    console.log('etalon.txt is writed');
-    execCode(run);
+test_system.on('etalonIsWrited', function(run){
+    //console.log('etalon.txt is writed');
+    this.execCode(run);
 });
 
 /**
- * Function for write input test in file. Completion of the function generates 
- * the event. This event triggers the function compareFiles().
- * @param run - number of current test case (1, 2, 3, ...).
+ * Function for execute client's code. If code isn't executed,
+ * function generates error event 'runtimeError'. Else function generates
+ * the event 'codeIsExecuted' and calls the function compareFiles().
+ * @param {type} run - number of current test case (1, 2, 3, ...).
  * @returns {undefined}
  */
-var execCode = function(run) {
-    exec(exec_command + ' ' + code + ' < ' + input + ' > ' + output,
+TestSystem.prototype.execCode = function(run) {
+    var solution = this.solution;
+    exec(this.exec_command + ' ' + this.code + ' < input.txt > output.txt',
     function(error, stdout, stderr){
         //console.log('error = ' + error + '\n');
         //console.log('stdout = ' + stdout + '\n');
         //console.log('strerr = ' + stderr + '\n');
-	eventEmitter.emit('codeIsExecuted', run);
+        if(stderr === '')
+            test_system.emit('codeIsExecuted', run);
+        else {
+            var error = new Error('runtime error');
+            error.name = 'Runtime Error';
+            error.message = 'Solution #' + solution + ' is incorrect.';
+            error.solution = solution;
+            test_system.emit('runtimeError', error);
+        }
     });
 };
-eventEmitter.on('codeIsExecuted', function(run){
-    console.log('code is executed');
-    compareFiles(run);
+test_system.on('codeIsExecuted', function(run){
+    //console.log('code is executed');
+    this.compareFiles(run);
 });
 
 /**
- * Function for compare result of client's code and etalon data. Completion of 
- * the function generates the event. This event display the result of testing
- * and start new test.
- * @param run - number of current test case (1, 2, 3, ...).
+ * Function for compare result of client's code and etalon data. 
+ * If files are not the same, then function generates error event 'testFailed'.
+ * Else function generates the event 'filesAreCompared' and starts new test.
+ * @param {type} run - number of current test case (1, 2, 3, ...).
  * @returns {undefined}
  */
-var compareFiles = function(run) {
-    exec('diff ' + output + ' ' + etalon,
+TestSystem.prototype.compareFiles = function(run) {
+    var solution = this.solution;
+    exec('diff output.txt etalon.txt',
     function(error, stdout, stderr){
         //console.log('error = ' + error + '\n');
         //console.log('stdout = ' + stdout + '\n');
         //console.log('strerr = ' + stderr + '\n');
-	eventEmitter.emit('filesAreCompared', stdout, run);
+        if(stdout === '')
+            test_system.emit('filesAreCompared', run);
+        else {
+            var error = new Error('test failed');
+            error.name = 'Test Failed';
+            error.message = 'Test #' + run + 
+                    ' for solution #' + solution + ' is fail.';
+            error.test = run;
+            error.solution = solution;
+            test_system.emit('testFailed', error);
+        }
     });
 };
-eventEmitter.on('filesAreCompared', function(stdout, run){
-    console.log('files are compared');
-    console.log('result:');
-    if(stdout === '') {
-        console.log('Test #' + run + ' passed!');
-        status = 'Test #' + run + ' passed!';
-        // start next test
-        run++;
-        if(run <= n_tests)
-            writeInput(run);
-    }
-    else {
-        console.log('Test #' + run + ' failed!');
-        status = 'Test #' + run + ' failed!';
-    }
-    
+test_system.on('filesAreCompared', function(run){
+    //console.log('files are compared');
+    // start next test
+    run++;
+    if(run <= this.n_tests)
+        this.writeInput(run);
+    else
+        test_system.emit('success');
 });
 
-// testing this JS code
-testing(1, code);
+// export object of class to other module
+module.exports = test_system;
