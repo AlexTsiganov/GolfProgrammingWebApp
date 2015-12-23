4ae3c13ee21d5453440adf3bf3687337ca79a98b
Zhuk Pavel <ppzhuk@gmail.com>
2015-12-23 04:42:29 +0300

    start to implement login/logout. not finished yet

--- libs/user/user.js
+++ libs/user/user.js
@@ -2,9 +2,14 @@ var crypto = require('crypto');
 var dataManager = require('../../Database/DataManager');
 var log = require('../log/log')(module);
 
-exports.addNewUser = function(name, plainPass) {
+var addNewUser = function(name, plainPass, callback) {
     var salt = Math.random() + '';
     var hashedPassword = crypto.createHmac('sha1', salt).update(plainPass).digest('hex');
 
-    log.debug("salt - " + salt + " hashPass - " + hashedPassword);
+    log.debug('salt: ' + salt + " hash_pass: " + hashedPassword);
+
+    dataManager.writeUser(name, salt, hashedPassword, callback);
+
 };
+
+exports.addNewUser = addNewUser;
